c88062a4ce4046c9c41e67b1c2165a1731950717
aptennn <uegor@protonmail.com>
2023-11-13 21:45:59 +0300

    Add TrainerScreen and add come changes

--- lib/presentation/pages/home_screen/home_screen.dart
+++ lib/presentation/pages/home_screen/home_screen.dart
@@ -43,7 +43,7 @@ class HomeScreen extends StatelessWidget {
           ),
           Positioned(
             bottom: 400.h,
-            child: trainerButton,
+            child: const TrainerButtonWidget(),
           ),
         ],
       ),
--- lib/presentation/pages/home_screen/widgets/trainer_button.dart
+++ lib/presentation/pages/home_screen/widgets/trainer_button.dart
@@ -1,35 +1,60 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:study_ready/presentation/pages/trainer_screen/trainer_screen.dart';
 import 'package:study_ready/utils/app_svg_assets.dart';
 
 
-final trainerButton = IconButton(
-  onPressed: () {},
-   splashColor: Colors.transparent,
-  highlightColor: Colors.transparent,
-  padding: const EdgeInsets.all(0),
-  icon: SizedBox(
+
+class TrainerButtonWidget extends StatelessWidget {
+  const TrainerButtonWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return     IconButton(
+      onPressed: () {
+        Navigator.of(context).pushReplacement(
+          PageRouteBuilder(
+            transitionDuration: Duration.zero,
+            pageBuilder: (context, animation, secondaryAnimation) {
+              return const TrainerScreen();
+            },
+            transitionsBuilder:
+                (context, animation, secondaryAnimation, child) {
+              return child;
+            },
+          ),
+        );
+      },
     
-    width: 375.w,
-    child: Stack(
-      children: [
-        Positioned(
-          child: sLetterBack,
+      splashColor: Colors.transparent,
+      highlightColor: Colors.transparent,
+      padding: const EdgeInsets.all(0),
+      icon: SizedBox(
+    
+        width: 375.w,
+        child: Stack(
+          children: [
+            Positioned(
+              child: sLetterBack,
+            ),
+            Positioned(
+              child: sLetterFront,
+            ),
+            Positioned(
+              left: 121.72.w,
+              bottom: 187.91.h,
+              child: trainerText,
+            ),
+            Positioned(
+              left: 165.87.w,
+              bottom: 143.2.h,
+              child: tudyText,
+            ),
+          ],
         ),
-        Positioned(
-          child: sLetterFront,
-        ),
-        Positioned(
-          left: 121.72.w,
-          bottom: 187.91.h,
-          child: trainerText,
-        ),
-        Positioned(
-          left: 165.87.w,
-          bottom: 143.2.h,
-          child: tudyText,
-        ),
-      ],
-    ),
-  ),
-);
+      ),
+    );
+  }
+}
