1e320c6f2f3e7f420354fc1eaa76b38bd638a2b5
Daniil Kuznetsov <daniil_161@inbox.ru>
2023-11-13 22:00:24 +0300

    add navigation to navigation screen

--- lib/presentation/pages/trainer_screen/trainer_screen.dart
+++ lib/presentation/pages/trainer_screen/trainer_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:study_ready/presentation/navigation/navigation_bar.dart';
 import 'package:study_ready/presentation/pages/trainer_screen/widget/trainer_card.dart';
 import 'package:study_ready/utils/app_colors.dart';
 import 'widget/app_bar_widget.dart'; // Импорт файла для AppBar
@@ -17,6 +18,7 @@ class _TrainerScreenState extends State<TrainerScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: const NavigatorDrawer(),
       backgroundColor: backgroundColor,
       body: Stack(children: [
         CustomScrollView(
--- lib/presentation/pages/trainer_screen/widget/app_bar_widget.dart
+++ lib/presentation/pages/trainer_screen/widget/app_bar_widget.dart
@@ -8,16 +8,23 @@ class AppBarWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SliverAppBar(
-      backgroundColor: backgroundColor,
+        backgroundColor: backgroundColor,
         pinned: true,
         snap: true,
         floating: true,
         surfaceTintColor: Colors.transparent,
         leading: Padding(
           padding: EdgeInsets.only(left: 20.w),
-          child: Icon(
-            Icons.menu,
-            size: 30.sp,
+          child: IconButton(
+            icon: Icon(
+              Icons.menu,
+              size: 40.sp,
+              weight: 10.w,
+            ),
+            onPressed: () {
+              Scaffold.of(context).openDrawer();
+            },
+            tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
           ),
         ),
         title: Padding(
