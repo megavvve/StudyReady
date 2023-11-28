1f8ed38957b2c8e8bb0a967627cf6b88ca3f90d7
Michael <87703210+FbGod@users.noreply.github.com>
2023-11-28 17:48:10 +0300

    Second trainer parsed and inserted to db

--- lib/domain/database/initialize/first_launch.dart
+++ lib/domain/database/initialize/first_launch.dart
@@ -16,47 +16,57 @@ class FillTables {
     fillChapters();
     fillThemes();
     var trainer = await loadJsonAsset();
+    // print(trainer);
+    // print(trainer[0]);
+    // print(trainer[1]);
     fillQuestions(trainer);
     fillTrainer(trainer);
   }
 
   // Json decode
   Future<List> loadJsonAsset() async {
-    final String jsonString =
+    final String jsonString1 =
         await rootBundle.loadString('assets/jsons/trainer1.json');
 
-    final data = jsonDecode(jsonString);
+    final String jsonString2 =
+        await rootBundle.loadString('assets/jsons/trainer2.json');
 
-    final trainer = data['trainers'];
+    final data1 = jsonDecode(jsonString1);
+    final data2 = jsonDecode(jsonString2);
 
-    return trainer;
+    final trainer1 = data1['trainers'];
+    final trainer2 = data2['trainers'];
+
+    return [trainer1, trainer2];
   }
 
   Future<void> fillQuestions(trainer) async {
     var listOfQuestions = await db.getQuestionsFullInfo();
     if (listOfQuestions.isEmpty) {
-      final questions = trainer[0]['questions'];
-      for (int i = 0; i < questions.length; i++) {
+      for (int j = 0; j < trainer.length; j++) {
+        final questions = trainer[j][0]['questions'];
+        for (int i = 0; i < questions.length; i++) {
         final incorrectAns = (questions[i]['incorrectAnswers'] as List)
             .map((e) => e as String)
             .toList();
         var question = QuestionCompanion(
-            courseNumber: drift.Value(questions[i]['courseNumber']),
-            subjectId: drift.Value(questions[i]['subjectId']),
-            chapterId: drift.Value(questions[i]['chapterId']),
-            themeId: drift.Value(questions[i]['themeId']),
-            difficultly: drift.Value(questions[i]['difficultly']),
-            questionContext: drift.Value(questions[i]['questionContext']),
-            rightAnswer: drift.Value(questions[i]['rightAnswer']),
-            incorrectAnswers: drift.Value(incorrectAns));
+        courseNumber: drift.Value(questions[i]['courseNumber']),
+        subjectId: drift.Value(questions[i]['subjectId']),
+        chapterId: drift.Value(questions[i]['chapterId']),
+        themeId: drift.Value(questions[i]['themeId']),
+        difficultly: drift.Value(questions[i]['difficultly']),
+        questionContext: drift.Value(questions[i]['questionContext']),
+        rightAnswer: drift.Value(questions[i]['rightAnswer']),
+        incorrectAnswers: drift.Value(incorrectAns));
 
         db.insertQuestion(question);
-      }
-    }
+        }
+     }
+  }
 
     // Debug print
 
-    //var listOfQuestionsDebug = await db.getQuestionsFullInfo();
+    // var listOfQuestionsDebug = await db.getQuestionsFullInfo();
     // for (var i = 0; i < listOfQuestionsDebug.length; i++) {
     //   print((listOfQuestionsDebug[i].id, listOfQuestionsDebug[i].course,
     //   listOfQuestionsDebug[i].subject, listOfQuestionsDebug[i].chapter, listOfQuestionsDebug[i].theme,
@@ -67,52 +77,56 @@ class FillTables {
   Future<void> fillTrainer(trainer) async {
     var listOfTrainers = await db.getTrainers();
     if (listOfTrainers.isEmpty) {
-      final questions = trainer[0]['questions'];
-      List<String> questionsIds = [];
-      for (int i = 0; i < questions.length; i++) {
-        questionsIds.add((questions[i]['id']).toString());
-      }
-      questionsIds = (questionsIds).map((e) => e).toList();
-      var trainerDB = TrainersCompanion(
-          name: drift.Value(trainer[0]['name']),
-          color: drift.Value(trainer[0]['color']),
-          image: drift.Value(trainer[0]['image']),
-          questions: drift.Value(questionsIds));
+      for (int j = 0; j < trainer.length; j++) {
+        final questions = trainer[j][0]['questions'];
+        List<String> questionsIds = [];
+        for (int i = 0; i < questions.length; i++) {
+          questionsIds.add((questions[i]['id']).toString());
+        }
+        questionsIds = (questionsIds).map((e) => e).toList();
+        var trainerDB = TrainersCompanion(
+            name: drift.Value(trainer[j][0]['name']),
+            color: drift.Value(trainer[j][0]['color']),
+            image: drift.Value(trainer[j][0]['image']),
+            questions: drift.Value(questionsIds));
 
-      db.insertTrainer(trainerDB);
+        db.insertTrainer(trainerDB);
+      }
     }
 
     var listOfTrainersDebug = await db.getTrainers();
-    print(listOfTrainersDebug);
+    //print(listOfTrainersDebug);
 
-    var questionsInTrainer1 = listOfTrainersDebug[0];
+    var questionsInTrainers = listOfTrainersDebug;
 
-    var trainerComplete =
-        await db.getTrainerFullInfoById(questionsInTrainer1.id);
+    for (int i = 0; i < questionsInTrainers.length; i++) {
+      var trainerComplete =
+      await db.getTrainerFullInfoById(questionsInTrainers[i].id);
 
-    var trainerCompleteQuestions = [];
+      var trainerCompleteQuestions = [];
 
-    for (int i = 0; i < trainerComplete.questions.length; i++) {
-      trainerCompleteQuestions.add((
-        trainerComplete.questions[i].id,
-        trainerComplete.questions[i].course,
-        trainerComplete.questions[i].subject,
-        trainerComplete.questions[i].chapter,
-        trainerComplete.questions[i].theme,
-        trainerComplete.questions[i].difficultly,
-        trainerComplete.questions[i].context,
-        trainerComplete.questions[i].rightAnswer,
-        trainerComplete.questions[i].incorrectAnswers
-      ));
+      for (int j = 0; j < trainerComplete.questions.length; j++) {
+        trainerCompleteQuestions.add((
+        trainerComplete.questions[j].id,
+        trainerComplete.questions[j].course,
+        trainerComplete.questions[j].subject,
+        trainerComplete.questions[j].chapter,
+        trainerComplete.questions[j].theme,
+        trainerComplete.questions[j].difficultly,
+        trainerComplete.questions[j].context,
+        trainerComplete.questions[j].rightAnswer,
+        trainerComplete.questions[j].incorrectAnswers
+        ));
+      }
+
+      print([
+        trainerComplete.id,
+        trainerComplete.name,
+        trainerComplete.color,
+        trainerComplete.image,
+        trainerCompleteQuestions
+      ]);
     }
-
-    print([
-      trainerComplete.id,
-      trainerComplete.name,
-      trainerComplete.color,
-      trainerComplete.image,
-      trainerCompleteQuestions
-    ]);
   }
 
   // creating test subjects
@@ -129,8 +143,8 @@ class FillTables {
       }
     }
 
-    var listOfSubjectsDebug = await db.getSubjects();
-    print(listOfSubjectsDebug);
+    // var listOfSubjectsDebug = await db.getSubjects();
+    // print(listOfSubjectsDebug);
   }
 
   // creating test chapters
@@ -150,8 +164,8 @@ class FillTables {
       }
     }
 
-    var listOfChaptersDebug = await db.getChapters();
-    print(listOfChaptersDebug);
+    // var listOfChaptersDebug = await db.getChapters();
+    // print(listOfChaptersDebug);
   }
 
   // creating test themes
@@ -159,9 +173,9 @@ class FillTables {
   void fillThemes() async {
     var listOfThemes = await db.getThemes();
     if (listOfThemes.isEmpty) {
-      var subjectIds = [1];
-      var chapterNames = [1];
-      var names = ['Основные обозначения'];
+      var subjectIds = [1, 1];
+      var chapterNames = [1, 1];
+      var names = ['Основные обозначения', 'Абсолютная величина'];
 
       for (int i = 0; i < subjectIds.length; i++) {
         var theme = ThemesCompanion(
@@ -173,7 +187,7 @@ class FillTables {
       }
     }
 
-    var listOfThemesDebug = await db.getThemes();
-    print(listOfThemesDebug);
+    // var listOfThemesDebug = await db.getThemes();
+    // print(listOfThemesDebug);
   }
 }
