21d8b93515f36db6d32471244ffb6dad4bcf5301
Ivan <sakopov@sfedu.ru>
2024-04-26 12:54:23 +0300

    CHANGE: min Android version, ru locale and migration on hydrated_bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart';
+
 import 'package:study_ready/firebase_options.dart';
 import 'package:study_ready/injection_container.dart';
 import 'package:study_ready/presentation/blocs/study_material_bloc/study_material_bloc.dart';
@@ -13,6 +17,11 @@ import 'package:study_ready/utils/app_themes.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  HydratedBloc.storage = await HydratedStorage.build(
+    storageDirectory: kIsWeb
+        ? HydratedStorage.webStorageDirectory
+        : await getTemporaryDirectory(),
+  );
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
@@ -56,6 +65,7 @@ class MyApp extends StatelessWidget {
         splitScreenMode: true,
         builder: (_, child) => MaterialApp(
           debugShowCheckedModeBanner: false,
+          locale: const Locale('ru'),
           title: 'StudyReady',
           theme: lightTheme,
           home: child,
